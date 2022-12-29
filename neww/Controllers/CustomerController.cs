8fd6539e2d950ca8256d21273ad1614dc2901eeb
EnisAsani <enkotrd@gmail.com>
2022-12-29 16:46:53 +0100

    update

---
+++ neww/Controllers/CustomerController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using neww.Models;
+
+namespace neww.Controllers
+    
+{
+    public class CustomerController : Controller
+    {
+        
+        [HttpGet]
+        public ActionResult AddorEdit()
+        {
+            Customer newCustomer = new Customer();
+
+            return View(newCustomer);
+        }
+        [HttpPost]
+        public ActionResult AddorEdit(Customer newCustomer)
+        {
+            using(DBModel dbmodel= new DBModel())
+            {
+                if (dbmodel.Customers.Any(x=> x.UserId == newCustomer.UserId))
+                {
+                    ViewBag.ErrorMessage = "Please enter another ID, that one is already taken.";
+                    return View("AddorEdit", newCustomer);
+                }
+                dbmodel.Customers.Add(newCustomer);
+                dbmodel.SaveChanges();
+            }
+            ModelState.Clear();
+            ViewBag.SuccessMessage = "Info Saved Successfully";
+            return View("AddorEdit", new Customer());
+        }
+    }
+}
